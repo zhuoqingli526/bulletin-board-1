b2ec9fc960565235b5896f893d5ee5b8099d0c6f
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-05-13 06:32:26 +0000

    first+draft

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,16 +19,16 @@ class PostsController < ApplicationController
 
   def create
     the_post = Post.new
-    the_post.title = params.fetch("query_title")
-    the_post.body = params.fetch("query_body")
-    the_post.expires_on = params.fetch("query_expires_on")
+    the_post.title = params.fetch("title")
+    the_post.body = params.fetch("body")
+    the_post.expires_on = params.fetch("expires_on")
     the_post.board_id = params.fetch("query_board_id")
 
     if the_post.valid?
       the_post.save
-      redirect_to("/posts", { :notice => "Post created successfully." })
+      redirect_to("/board/#{the_post.board_id}", { :notice => "Post created successfully." })
     else
-      redirect_to("/posts", { :alert => the_post.errors.full_messages.to_sentence })
+      redirect_to("/board/#{the_post.board_id}", { :alert => the_post.errors.full_messages.to_sentence })
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get("/", { :controller => "boards", :action => "index" })
   # Routes for the Post resource:
 
   # CREATE
